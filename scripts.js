9abc4a95a224dde84626ec549409ca5177a00009
Gudhaf <52878976+Gudhaf@users.noreply.github.com>
2020-01-20 12:34:03 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,4 @@
-$("input.box").each(function() {
-    var mycookie = $.cookie($(this).attr('name'));
-    if (mycookie && mycookie == "true") {
-        $(this).prop('checked', mycookie);
-    }
-});
-$("input.box").change(function() {
-    $.cookie($(this).attr("name"), $(this).prop('checked'), {
-        path: '/',
-        expires: 365
-    });
-});
+//on page load set the checkbox to stored value or default to true
+$('#box').prop('checked' , ( typeof sessionStorage.box !== 'undefined' ) ? (sessionStorage.box=='true') : true ); 
+//when checkbox is updated, update stored value
+$('#box').change( function() { sessionStorage.box = $(this).prop('checked');  }); 
