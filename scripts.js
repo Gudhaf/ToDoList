81cbfebc4d3f6a0d34522505f6873d451a18f40b
Gudhaf <52878976+Gudhaf@users.noreply.github.com>
2020-01-20 12:34:48 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 //on page load set the checkbox to stored value or default to true
-$('#box').prop('checked' , ( typeof sessionStorage.box !== 'undefined' ) ? (sessionStorage.box=='true') : true ); 
+$('#box').prop('checked' , ( typeof localStorage.box !== 'undefined' ) ? (localStorage.box=='true') : true ); 
 //when checkbox is updated, update stored value
-$('#box').change( function() { sessionStorage.box = $(this).prop('checked');  }); 
+$('#box').change( function() { localStorage.box = $(this).prop('checked');  }); 
